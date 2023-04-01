ca984c1deb30eb6f7cda91a44cba4b57f9554766
kirituu <127484290+kirituu@users.noreply.github.com>
2023-04-01 13:43:48 +0300

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,14 @@
+//single quoted
+let singleSring = 'Hello, World!';
+
+//double quoted 
+let doubleString = "Hello, World!";
+
+//Template literals
+let name = "Alice";
+let string = `Hello, ${name}!`;
+
+//String methods
+let string4 = "hello, world!";
+let string5 = string4.toUpperCase();
+let string6 = string4.slice(0, 5); 
