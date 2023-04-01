2b6816f25c7e22e93b4049ba80edbb976aa6e371
kirituu <127484290+kirituu@users.noreply.github.com>
2023-04-01 16:37:25 +0300

    Add files via upload

---
+++ arithmetic lab (1).js
@@ -0,0 +1,21 @@
+
+// Multiply num1 and num2 to get 62
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+console.log(multiply); 
+
+// Generating a random integer greater than 0
+const random = Math.floor(Math.random() * 100) + 1;
+console.log(random); 
+
+// Calculating the remainder of dividing num3 by num4 to get 4
+const num3 = 14;
+const num4 = 5;
+const mod = num3 % num4;
+console.log(mod);
+
+// Finding the highest number in a set
+const numbers = [5, 10, 15, 20];
+const max = Math.max(...numbers);
+console.log(max);
