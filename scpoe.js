926375633fcdfb32fca1c4eb685c89e7f52ba2bd
kirituu <127484290+kirituu@users.noreply.github.com>
2023-04-02 23:34:36 +0300

    Add files via upload

---
+++ scpoe.js
@@ -0,0 +1,20 @@
+let customerName = "daniel";
+
+function upperCaseCustomerName() {
+  return customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  bestCustomer = "not maina";
+}
+
+function overwriteBestCustomer() {
+  bestCustomer = "definitely not maina";
+}
+
+const leastFavoriteCustomer = "kiritu";
+
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = "martin";
+}
